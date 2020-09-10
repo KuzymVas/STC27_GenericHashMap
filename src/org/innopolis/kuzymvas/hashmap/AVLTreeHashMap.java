17b75ccaa3260d3816437f15447940a90858511b
Vasiliy Kuzmin <kuzymvas@yandex.ru>
2020-09-10 09:37:44 +0300

    + Тесты для хэш таблицы на АВЛ деревьях + Пустая реализация AVLTreeHashMap

---
+++ src/org/innopolis/kuzymvas/hashmap/AVLTreeHashMap.java
@@ -0,0 +1,42 @@
+package org.innopolis.kuzymvas.hashmap;
+
+public class AVLTreeHashMap implements HashMap {
+
+
+    public AVLTreeHashMap(int bucketNumber) {
+    }
+
+    public AVLTreeHashMap() {
+
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+
+    }
+
+    @Override
+    public void replace(Object key, Object value) throws KeyNotPresentException {
+
+    }
+
+    @Override
+    public Object get(Object key) throws KeyNotPresentException {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) throws KeyNotPresentException {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
